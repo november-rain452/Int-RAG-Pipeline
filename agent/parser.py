7672270225e621ca57df0e4d4cc6ad35f29b738a
Aditya Sinha <ranjanaditya452@gmail.com>
2026-05-24 23:56:52 +0530

    updated parser to include validation logic

--- agent/parser.py
+++ agent/parser.py
@@ -1,9 +1,11 @@
 import json
+from schema.agent_json import ResponseSchema
 
 
 def parse_response(response: str):
     try:
-        return json.loads(response)
+        validated_data = ResponseSchema.model_validate_json(response)
+        return validated_data.model_dump()
 
     except:
         return {"action": "answer", "data": "Parsing failed."}
