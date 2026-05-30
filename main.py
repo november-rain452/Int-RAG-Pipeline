c9768e9e13aa4f44bd33d8b28a565f4b834d8635
Aditya Sinha <ranjanaditya452@gmail.com>
2026-05-30 23:58:35 +0530

    code to ingest data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from agent.loop import run_agent
+from rag.ingest import ingest
 
 if __name__ == "__main__":
+
+    ingest("data/")
+
     while True:
         query = input(">> ")
         run_agent(query)
